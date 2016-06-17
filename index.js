475aa0f50e54d8a8fd52b0d785149a5c284bc7d9
yeliex <yeliex@yeliex.com>
2016-06-17 12:06:00 +0800

    WIP.

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Creator: yeliex
+ * Project: rkjsbridge
+ * Description:
+ */
+
+(function () {
+
+  const init = function ({ protocol, domain, port }) {
+
+  };
+
+  const invoke = function invoke(type, name, params, callback) {
+
+  }
+
+
+}());
